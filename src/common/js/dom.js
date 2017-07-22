0401f0a7059a13a7bb828c65a7b440a21fd39091
JasonHJ <hejian_best@163.com>
2017-07-22 18:31:59 +0800

    classified song

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -6,6 +6,12 @@ export function addClass(el, className) {
   el.className = newClass.join(' ');
 }
 
+export function removeClass(el,className) {
+  if(!hasClass(el,className))
+    return;
+
+}
+
 export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
   return reg.test(el.className);
@@ -18,4 +24,19 @@ export function splitArr(arr, num) {
     newArr.push(arr.slice(i, i + num));
   }
   return newArr;
+}
+
+export function getData(el,name,val) {
+	const prefix="data-";
+	name=prefix+name;
+	if(val)
+		return el.setAttribute(name,val);
+	else
+		return el.getAttribute(name);
+}
+
+export function onlyClass(el,className) {
+	let parent=el.parentElement;
+  parent.getElementsByClassName(className)[0].className="item"
+  addClass(el,className);
 }
